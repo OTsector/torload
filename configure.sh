12de4d635f7f7ff94f4f5fa095dbbe592a543fa5
OT <imot@protonmail.com>
2017-09-03 23:33:35 +0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo apt-get install proxychains tor -y
-sudo cp -R torload pc /usr/bin -f
-sudo chmod +x /usr/bin/torload
+sudo cp -R pc myip torload /usr/bin -f
 sudo chmod +x /usr/bin/pc
+sudo chmod +x /usr/bin/myip
+sudo chmod +x /usr/bin/torload
+clear
 echo "Done! -Now? ... Nobody's win!"
 sudo rm -rf ../torload
 exit 0
