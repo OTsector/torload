808482d63cbf3b6bdb704aebb02af2b961d6176b
OT <imot@protonmail.com>
2019-12-19 11:25:48 -0500

    Update myip.sh

--- myip.sh
+++ myip.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'
+curl -s -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0' \
+	checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'
 exit 0
