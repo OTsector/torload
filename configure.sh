a2ee92bcfe01bb6c8c59d59aa9ecd915bd07ede4
OT <imot@protonmail.com>
2020-02-07 06:08:34 +0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install proxychains tor curl -y
+sudo apt-get install proxychains tor curl tor-resolve -y
 sudo cp -f /usr/bin/proxychains /usr/bin/pc
 sudo sed 's/echo "ProxyChains/#echo "ProxyChains/g' -i /usr/bin/pc
 sudo cp myip.sh /usr/bin/myip
 sudo cp torload.sh /usr/bin/torload
+sudo cp pcnmap.sh /usr/bin/pcnmap
 sudo chmod +x /usr/bin/pc
 sudo chmod +x /usr/bin/myip
 sudo chmod +x /usr/bin/torload
