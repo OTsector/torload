d0ee573da8448526646cd355850559f0a62f6f0a
OT <imot@protonmail.com>
2020-02-29 16:14:00 +0400

    Add files via upload

--- pcnmap.sh
+++ pcnmap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-	nmap --help|sed 's/nmap/pcnmap/g'
+	nmap --help|sed 's/nmap /pcnmap /g'
 	exit 1
 fi
 
@@ -27,9 +27,9 @@ fi
 if [[ ${hostname:${#hostname}-6} == ".onion" ]]; then
 	host=$hostname
 	torsocks 2>/dev/null nmap -sT -Pn -n $argv $host \
-		|sed 's/nmap/pcnmap/g'
+		|sed 's/nmap /pcnmap /g'
 else
 	host=$(tor-resolve $hostname)||exit 1
 	pc nmap -sT -Pn -n $argv $host \
-		|sed 's/nmap/pcnmap/g'
+		|sed 's/nmap /pcnmap /g'
 fi
