59950a2a0a1802d6deb0b22f00fff833ce93ea65
OT <imot@protonmail.com>
2020-10-28 22:51:39 +0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,10 +7,12 @@ sudo sed 's/echo "ProxyChains/#echo "ProxyChains/g' -i /usr/bin/pc
 sudo cp myip.sh /usr/bin/myip
 sudo cp torload.sh /usr/bin/torload
 sudo cp pcnmap.sh /usr/bin/pcnmap
+sudo cp subdomain.sh /usr/bin/subdomain
 sudo chmod +x /usr/bin/pc
 sudo chmod +x /usr/bin/myip
 sudo chmod +x /usr/bin/torload
 sudo chmod +x /usr/bin/pcnmap
+sudo chmod +x /usr/bin/subdomain
 sudo sed 's/#quiet_mode/quiet_mode/g' -i /etc/proxychains.conf
 # sudo sed 's/proxy_dns/#proxy_dns/g' -i /etc/proxychains.conf
 clear
